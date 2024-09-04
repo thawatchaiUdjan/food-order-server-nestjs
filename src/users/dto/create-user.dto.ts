9b491e1c700bab7285b77fc422ae1bd19a7955ff
thawatchaiUdjan <thawatchai_udjan@outlook.com>
2024-09-04 10:48:41 +0700

    [UPDATE] add create user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,45 @@
-export class CreateUserDto {}
+import {
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  IsNumber,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly user_id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(45)
+  readonly username: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(100)
+  readonly password?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(45)
+  readonly role?: string;
+
+  @IsNumber()
+  @IsOptional()
+  readonly balance?: number;
+
+  @IsOptional()
+  readonly location?: {
+    readonly address?: string;
+    readonly latlng?: {
+      readonly lat?: number;
+      readonly lng?: number;
+    };
+  };
+}
