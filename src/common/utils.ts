d0bc6373ef4279473b3d46210b420fb9f5af0eb2
thawatchaiUdjan <thawatchai_udjan@outlook.com>
2024-09-06 11:59:20 +0700

    [FIX] add config service to use

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,11 +1,12 @@
+import { ConfigService } from '@nestjs/config';
 import * as bcrypt from 'bcrypt';
 import { v4 as uuidv4 } from 'uuid';
 
-export async function hashPassword(
-  password: string,
-  saltRound: number,
-): Promise<string> {
-  const salt = await bcrypt.genSalt(saltRound);
+const configService = new ConfigService();
+
+export async function hashPassword(password: string): Promise<string> {
+  const saltRounds = configService.get<number>('encryptSaltRounds');
+  const salt = await bcrypt.genSalt(saltRounds);
   return bcrypt.hash(password, salt);
 }
 
